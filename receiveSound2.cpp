e68581f2fb333c80769d06a47d33d70de6092037
SpeccyPlays <102996742+SpeccyPlays@users.noreply.github.com>
2022-08-15 13:51:51 +0100

    Update receiveSound2.cpp
    Uses  SDL to play the audio data it receives
    No popping sounds on playback (an issues in the java version)

--- receiveSound2.cpp
+++ receiveSound2.cpp
@@ -2,17 +2,36 @@
 Receives and saves the raw audio info from an ESP32 taking samples from a MAX4466 microphone
 Output file will open in Audacity as unsigned 8-bit PCM, single channel at 11,025hz (or similar)
 endianness doesn't matter
-*/
+Liveplays the data it receives
 
+*/
+#include <SDL.h>
+#include <SDL_audio.h>
+#undef main //otherwise it expects main to be called SDL_main
 #include <SFML/Network.hpp>
 #include <iostream>
 #include <fstream>
+
 #define BUFFERSIZE 1024
 
+void myAudioCallBack(void *userdata, uint8_t *stream, int len);
+uint8_t receiveBuffer[BUFFERSIZE] = {0};
 
 int main (){
-    uint8_t soundData[BUFFERSIZE];
     std::ofstream outfile;
+    std::ofstream outfile16;
+    // setup all audio settings
+    SDL_Init(SDL_INIT_AUDIO);
+    SDL_AudioSpec audiospec;
+    audiospec.freq = 11025;
+    audiospec.format = AUDIO_U8;
+    audiospec.channels = 1;
+    audiospec.silence = 127;
+    audiospec.samples = BUFFERSIZE;
+    audiospec.callback = myAudioCallBack;
+    audiospec.userdata = NULL;
+    SDL_OpenAudio(&audiospec, NULL); //pass as reference needed
+
     while(1){
         //open file
         outfile.open("adc.raw", std::ios_base::out | std::ios::app | std::ios::binary);
@@ -24,23 +43,29 @@ int main (){
         socket.setBlocking(true); //if not blocking it wasn't receiving all data
         listener.accept(socket);
         std::cout << "New client connected: " << socket.getRemoteAddress() << std::endl;
-        // Receive a message from the client
-        uint8_t receiveBuffer[BUFFERSIZE];
+        // Receive data from the client
         std::size_t received = 0;
         socket.receive(receiveBuffer, sizeof(receiveBuffer), received);
         std::cout << "Received :" << received << std::endl;
-        for (uint16_t count = 0; count < sizeof(receiveBuffer); count++){
-            soundData[count] = (int)receiveBuffer[count];// cast makes sure it's not copying binary data.
+        for (uint16_t count = 0; count < sizeof(receiveBuffer); count++ ){
+            uint8_t sample = (int)receiveBuffer[count]; //convert to int. Not sure if needed
             if (outfile.is_open()){
-                outfile << soundData[count];
+                outfile << sample;
             }
             else {
-                std::cout << "File didn't open" << std::endl;
+                std::cout << "File  didn't open" << std::endl;
             }
         }
         outfile.close();
+        SDL_PauseAudio(0);
     } //end while loop
+    SDL_Quit();
 return 0;
 } //end main
+
+void myAudioCallBack(void *userdata, uint8_t *stream, int len){
+    //called when it's reached playing the end of the audio buffer
+    memcpy(stream, &receiveBuffer, sizeof(receiveBuffer));
+}
 
 
